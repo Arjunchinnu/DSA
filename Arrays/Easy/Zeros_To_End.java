915422c056326493cd5cadd607d82f20c6fd2208
arjunchinnu604@gmail.com <user.email like : "arjunchinnu604@gmail.com>
2026-05-16 20:48:01 +0530

    easy array

--- Arrays/Easy/Zeros_To_End.java
+++ Arrays/Easy/Zeros_To_End.java
@@ -1,3 +1,13 @@
+// Move all Zeros to the end of the array
+
+
+// Problem Statement: You are given an array of integers, your task is to move all the zeros in the array to the end of the array and move non-negative integers to the front by maintaining their order.
+
+// Examples
+// Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
+// Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
+// Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
+
 import java.util.Arrays;
 
 public class Zeros_To_End {
